89efdfb5c472f35d947bcdb02c97a686b4d85269
Jürgen Weichand <juergen@weichand.de>
2015-07-07 19:16:05 +0200

    [LDBV] bugfix für XML ohne schemaLocations

--- python/validate-xml.py
+++ python/validate-xml.py
@@ -12,7 +12,7 @@ for dirpath, dirnames, files in os.walk(basedir):
     for name in files:
 
         # Überprüfung aller XML-Dateien
-        if name.lower().endswith('service.xml'):
+        if name.lower().endswith('.xml'):
             xmlfilename = (os.path.join(dirpath, name))
 
             # XML mit lxml verarbeiten
@@ -21,31 +21,34 @@ for dirpath, dirnames, files in os.walk(basedir):
 
 
                 # für lxml alle Schemata (schemaLocations) in einem temporären Schema vereinen (über Import)
-                locations = xmldoc.getroot().get("{http://www.w3.org/2001/XMLSchema-instance}schemaLocation").split()
-                xsddoc = etree.Element("schema", attrib={
-                        "elementFormDefault": "qualified",
-                        "version": "1.0.0",
-                    }, nsmap={
-                    None: "http://www.w3.org/2001/XMLSchema"
-                    }
-                )
-                # ergänze Imports
-                for i in range(0,len(locations)):
-                    if not i % 2:
-                        etree.SubElement(xsddoc, "import", attrib={
-                            "namespace": locations[i],
-                            "schemaLocation": locations[i+1]
-                            }
-                        )
 
-                # Validierung der XML-Datei gegen das temporäre Schema
-                try:
-                    print 'Validiere ' + xmlfilename + ' gegen ' + etree.tostring(xsddoc)
-                    xsd = etree.XMLSchema(etree.XML(etree.tostring(xsddoc)))
-                    xsd.assertValid(xmldoc)
-                except etree.DocumentInvalid as e:
-                    print(e)
-                    exit(1)
+                schemaLocation = xmldoc.getroot().get("{http://www.w3.org/2001/XMLSchema-instance}schemaLocation")
+                if schemaLocation:
+                    locations = schemaLocation.split()
+                    xsddoc = etree.Element("schema", attrib={
+                            "elementFormDefault": "qualified",
+                            "version": "1.0.0",
+                        }, nsmap={
+                        None: "http://www.w3.org/2001/XMLSchema"
+                        }
+                    )
+                    # ergänze Imports
+                    for i in range(0,len(locations)):
+                        if not i % 2:
+                            etree.SubElement(xsddoc, "import", attrib={
+                                "namespace": locations[i],
+                                "schemaLocation": locations[i+1]
+                                }
+                            )
+
+                    # Validierung der XML-Datei gegen das temporäre Schema
+                    try:
+                        print 'Validiere ' + xmlfilename + ' gegen ' + etree.tostring(xsddoc)
+                        xsd = etree.XMLSchema(etree.XML(etree.tostring(xsddoc)))
+                        xsd.assertValid(xmldoc)
+                    except etree.DocumentInvalid as e:
+                        print(e)
+                        exit(1)
 
 
 
